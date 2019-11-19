fd609ac6b507030cecb0af22f6fdab9e9d926881
Wilson, Marvin (jw9615) <jw9615@att.com>
2019-11-19 12:00:01 -0500

    write json to modifier_temp.json

--- lib/src/util/modifier_file_updater.dart
+++ lib/src/util/modifier_file_updater.dart
@@ -4,17 +4,13 @@ import 'package:gurps_modifiers/src/modifiers.dart';
 
 main() async {
   ModifierTemplates modifiers = ModifierTemplates.instance();
-  var contents = '''
-String modifierDataString = \'\'\'
-  ${modifiers.printSourceData()}
-\'\'\';
-''';
+  var contents = modifiers.printSourceData();
 
   print(contents);
 
   // Write file
   var fileCopy =
-      await File('lib/src/modifier_data.dart').writeAsString(contents);
+      await File('lib/src/modifier_temp.json').writeAsString(contents);
   print(await fileCopy.exists());
   print(await fileCopy.length());
 }
